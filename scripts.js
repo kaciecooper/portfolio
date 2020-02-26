76c1b2769a448c0a1b25d4646b88c933e1f43a7b
Kacie Cooper <kcrouch@pdx.edu>
2020-02-25 16:58:37 -0800

    added pages for portfolio

---
+++ scripts.js
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kacie Cooper</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://use.typekit.net/bin4bel.css">
+
+</head>
+
+
+<body>
+
+	<main>
+	
+
+	
+
+		<nav class="topnav" id="myTopnav">
+			<a href="#home" class="active">Contact</a>
+			 
+  		</nav> 
+
+	
+		<section class="intro">
+					Hello! I'm Kacie Cooper.
+
+				 	Graphic Designer, Maker, Thinker, and Photographer.
+				    Take a look at my work below
+		</section>	
+
+		<section class="opportunities">
+					> Currenly seeking opportunities to work.
+
+		</section>	
+
+		
+
+<section>
+			
+			<div class="work-row1">
+				<h2>
+					Client Work
+				</h2>	
+
+				<div class="scroll-section1">
+					<div class="campusrec">
+					</div>
+					
+					<div class="pacificsentinel">
+					</div>	
+
+					<div class="youthmusicproject">
+					</div>
+						
+				<div>	
+			</div>	
+		
+			<div class="work-row2">
+				<h2>
+					School Projects
+				</h2>	
+
+
+				<div class="scroll-section2">
+					<div class="forage">
+					</div>
+
+					<div class="harneysons">
+					</div>
+					
+					<div class="whales">
+					</div>	
+
+					<div class="revengeseries">
+					</div>		
+				<div>	
+
+
+			</div>	
+
+			<div class="work-row3">
+				<h2>
+					Photography
+				</h2>	
+
+				<div class="scroll-section3">
+					<div class="surreal">
+					</div>
+					
+					<div class="landscapes">
+					</div>	
+
+					<div class="other">
+					</div>		
+				<div>	
+
+			</div>	
+
+</section>
+
+
+
+
+
+
+	</main>
+		<footer class="footer">
+			<p>
+				Let's do some stuff together!
+
+				Based in Portland, Oregon.
+
+
+			&copy;Kacie Cooper 2020
+	</footer>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+	<script
+  	src="https://code.jquery.com/jquery-3.4.1.min.js"
+  	integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  	crossorigin="anonymous"></script>
+  	<!-- link for your script file -->
+  	<script src="scripts/script.js"></script>	
+
+
+</body>
+</html>	
