203dffd1c6282333db2f56ec3767d4cb2ef22811
Kacie Cooper <kcrouch@pdx.edu>
2020-08-24 11:18:49 -0700

    trying

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+	// init controller
+	var controller = new ScrollMagic.Controller({vertical: false});
+
+	// build tween
+	var tween = TweenMax.to("#target", 0.5, {backgroundColor: "green", width: "+=400"});
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 500})
+					.setTween(tween)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
