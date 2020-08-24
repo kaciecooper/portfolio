e13729a0d5f718128bd9b6a35a8909d79f7eb890
Kacie Cooper <kcrouch@pdx.edu>
2020-08-24 11:20:56 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,3 @@
-	// init controller
-	var controller = new ScrollMagic.Controller({vertical: false});
-
-	// build tween
-	var tween = TweenMax.to("#target", 0.5, {backgroundColor: "green", width: "+=400"});
-
-	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 500})
-					.setTween(tween)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-37524344-3', 'scrollmagic.io');
+ga('send', 'pageview');
