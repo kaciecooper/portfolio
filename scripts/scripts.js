c293207efe156e98d458b5db7152f0e788190b83
Kacie Cooper <kcrouch@pdx.edu>
2020-08-24 11:46:08 -0700

    trying again

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,57 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-37524344-3', 'scrollmagic.io');
-ga('send', 'pageview');
+// your script file for adding your own jquery
+$(function() {
+  // Your Code from here on down. Don't delete that line above!
+  ////////////////////////////////////////////////////////////////Perspective Script
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 50) {
+      $('.rightside').hide();
+    } else {
+      $('.rightside').show();
+    }
+  });
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 50) {
+      $('.upsidedown').show();
+    } else {
+      $('.upsidedown').hide();
+    }
+  });
+  ///////////////////////////////SECTION 1
+  // init controller
+  var controller = new ScrollMagic.Controller();
+  // build scene
+  new ScrollMagic.Scene({
+      triggerElement: "#trigger-good",
+      triggerHook: 1, // show, when scrolled 10% into view
+      duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+      offset: 50 // move trigger to center of element
+    }).setClassToggle("#reveal-good", "visible") // add class to reveal
+    .addTo(controller);
+  /////////////////////////////////////////////SECTION 3////////////////////////////////
+  // init controller
+  var controller = new ScrollMagic.Controller();
+  // build tween
+  var tween = TweenMax.to("#animate", 0.5, {
+    scale: 1.3,
+    repeat: 5,
+    yoyo: true
+  });
+  // build scene and set duration to window height
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger",
+    duration: "100%"
+  }).setTween(tween).addTo(controller);
+  /////////////////////////////////////////////////////////////////////////////
+  wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+  })
+  wow.init()
+  //////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////
+});
